943b0e3f3fa1def4d853b43b74905f53dadf0ae4
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-10-18 16:44:36 -0400

    first 3 done

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-function changeLast() {
+function changeLast(arr,num) {
+  arr[arr.length-1] = num;
+}
+const arr1 = [1, 2, 3];
+const arr2 = [1, 2];
+changeLast(arr1,5);
+changeLast(arr2,5);
 
+function addMeToEnd(arr) {
+  arr.push("Colin")
 }
 
-function addMeToEnd() {
+addMeToEnd(arr1)
+addMeToEnd(arr2)
 
+function addMeToStart(arr) {
+  arr.unshift("Colin")
 }
 
-function addMeToStart() {
+addMeToStart(arr1)
+addMeToStart(arr2)
 
+function changeAllValuesTo(arr,value) {
+  let i=0
+  while (i< arr.length) {
+  arr[i]= value;
+  }
 }
-
-function changeAllValuesTo() {
-
-}
+changeAllValuesTo(arr1,15)
+changeAllValuesTo(arr2,30)
 
 function oddOrEven() {
 
